6e44ffd6aedbd23a25708c3c76b5329ed5b5d474
yolanda martinez priego <yulielx@gmail.com>
2024-01-22 13:25:23 +0000

    small changes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,9 +5,9 @@ class QuestionsController < ApplicationController
   def answer
     # raise
     @answer = ''
-    if params[:question] == 'I am going to work'
+    if params[:question].capitalize == 'I am going to work'
       @answer = 'Great!'
-    elsif params[:question].end_with?('?')
+    elsif params[:question].capitalize.end_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
